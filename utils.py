8696ccecd1a4e66400a7497b706ecae7bdc35ff8
Mateus <mcostat@outlook.com>
2023-08-22 13:49:31 +0000

    Primeiro Commit

---
+++ utils.py
@@ -0,0 +1,10 @@
+
+POSTGRES_HOST = "db"
+POSTGRES_PORT = 5432
+POSTGRES_USER = "postgres"
+POSTGRES_DB = "postgres"
+POSTGRES_PASSWORD = "postgres"
+
+
+API_URL = "https://jsonplaceholder.typicode.com"
+
